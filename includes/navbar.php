69966e7bcea3fa8fd85389c7b195d742551fc01e
unknown <manuel.urak88@gmail.com>
2022-01-24 00:51:56 +0100

    started navbar

---
+++ includes/navbar.php
@@ -0,0 +1,64 @@
+<div class="navbar-container">
+    <div class="image-contain collapse-button">
+        <img src="assets/imgs/collapse-button.png">
+    </div>
+    <a href="index.php">
+        <div class="image--contain nav-logo">
+            <img src="assets/imgs/logo.jpg" alt="logo">
+        </div>
+    </a>
+    <nav class="navbar">
+        <ul>
+            <li>
+                <a href="index.php" class="no-style">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="shop.php" class="no-style">
+                    Shop
+                </a>
+            </li>
+            <li>
+                <a href="spices.php" class="no-style">
+                    Unsere Gewürze
+                </a>
+            </li>
+            <li>
+                <a href="appartement.php" class="no-style">
+                    Ferienwohnung
+                </a>
+            </li>
+            <li>
+                <a href="travel.php" class="no-style">
+                    Reisen
+                </a>
+            </li>
+            <li>
+                <a href="about-us.php" class="no-style">
+                    Über uns
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <div class="user-int-bar">
+        <ul>
+            <li>
+                <a href="my-account.php" class="no-style">
+                    Mein Account
+                </a>
+            </li>
+            <li>
+                <a href="login.php" class="no-style">
+                    Login
+                </a>
+            </li>
+            <li>
+                <a href="sign-up.php" class="no-style">
+                    Registrieren
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="helper"></div>
+</div>
